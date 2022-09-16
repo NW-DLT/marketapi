9f889b14d33cb83dea290517c2839f7f6fa5c5b1
NW_DLT <alexputil228@mail.ru>
2022-09-16 17:45:41 +0300

    Добавьте файлы проекта.

---
+++ AppDBContent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using back4ker4.Models;
+
+namespace back4ker4
+{
+    public class AppDBContent : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDBContent(DbContextOptions<AppDBContent> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Controllers/ProductssController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using back4ker4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back4ker4.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductssController : ControllerBase
+    {
+        private readonly AppDBContent _context;
+
+        public ProductssController(AppDBContent context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
